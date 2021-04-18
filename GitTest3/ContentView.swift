d496f4d7b36ef498476ca741911cecd30612bf55
David <blastodave@gmail.com>
2021-04-18 17:48:16 -0400

    Initial Commit

---
+++ GitTest3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTest3
+//
+//  Created by Davidi on 4/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitTest3/GitTest3App.swift
@@ -0,0 +1,17 @@
+//
+//  GitTest3App.swift
+//  GitTest3
+//
+//  Created by Davidi on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct GitTest3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
